71ccede2ec4d8532761663cf1e513a51fa74c7d9
Ryu Jung woo <magicwand10@naver.com>
2026-01-16 20:21:32 +0900

    [feat]: Resources 폴더 추가, gamemanager 정보 전달 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [SerializeField] private float _time;
+    public float time { get => _time; }
 
+    [SerializeField] private Recipe _chosenRecipe;
+    public Recipe chosenRecipe { get => _chosenRecipe; }
+
+    public void setRecipe(Recipe recipe) { 
+        _chosenRecipe = recipe;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
