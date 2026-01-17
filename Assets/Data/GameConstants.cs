14bfd238ed4a5caa2e5b53a7fd7512c80620b243
kimhee0129 <kimhee0129@gmail.com>
2026-01-17 16:53:27 +0900

    [chore] 중간 세이브

---
+++ Assets/Data/GameConstants.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameConstants", menuName = "Scriptable Objects/GameConstants")]
+public class GameConstants : ScriptableObject
+{
+    
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public float yOffset = 32.4f;
+    [SerializeField] Transform group1;
+    [SerializeField] Transform group2;
+
+    
+}
