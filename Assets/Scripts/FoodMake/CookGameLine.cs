41d6a1026f93b12bbd5518acd49bcfa51fc02d2d
mood173 <gangm2832@gmial.com>
2026-01-17 11:49:17 +0900

    [refactor]중간 선넘으면 재료 삭제

--- Assets/Scripts/FoodMake/CookGameLine.cs
+++ Assets/Scripts/FoodMake/CookGameLine.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CookGameLine : MonoBehaviour, IPointerEnterHandler
+public class CookGameLine : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     MouseHand mouseHand;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Hi");
+        if (mouseHand.Gethand() != null)
+            mouseHand.Gethand().GetComponent<IEntity>().SelfRelease();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
         if (mouseHand.Gethand() != null)
             mouseHand.Gethand().GetComponent<IEntity>().SelfRelease();
     }
