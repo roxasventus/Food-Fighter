dad70b71ffd3a4a98d5a1e20b55416d7e805ae6b
kimhee0129 <kimhee0129@gmail.com>
2026-01-17 17:22:43 +0900

    [feat] 배경 스크롤링 완성

--- Assets/Data/GameConstants.cs
+++ Assets/Data/GameConstants.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameConstants", menuName = "Scriptable Objects/GameConstants")]
 public class GameConstants : ScriptableObject
 {
-    
+    public float boardSpeed = 4f;
+    public float truckShakeX = 0.4f;
+    public float truckShakeY = 0.15f;
+    public float truckShakeFreq = 0.5f;
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,10 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
     public float yOffset = 32.4f;
+    private Vector2 originPos;
+    [SerializeField] GameConstants gc;
     [SerializeField] Transform group1;
     [SerializeField] Transform group2;
+    [SerializeField] Transform truck;
 
-    
+    void Start()
+    {
+        originPos = truck.position;
+
+        StartCoroutine(TruckShake());
+    }
+    void Update()
+    {
+        BoardScroll();
+    }
+
+    private void BoardScroll()
+    {
+        Vector3 delta = Vector3.down * gc.boardSpeed * Time.deltaTime;
+        group1.position += delta;
+        group2.position += delta;
+
+        if (group1.position.y < -yOffset * 0.9f)
+        {
+            group1.position += Vector3.up * yOffset * 2;
+        }
+
+        if (group2.position.y < -yOffset * 0.9f)
+        {
+            group2.position += Vector3.up * yOffset * 2;
+        }
+    }
+
+    IEnumerator TruckShake()
+    {
+        float elapsed = 0f;
+        float duration = gc.truckShakeFreq;
+        while (true)
+        {
+            elapsed = 0f;
+            Vector2 firstPos = truck.position;
+            Vector2 targetPos = new Vector2(
+                Random.Range(originPos.x - gc.truckShakeX, originPos.x + gc.truckShakeX),
+                Random.Range(originPos.y - gc.truckShakeY, originPos.y + gc.truckShakeY)
+            );
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+
+                truck.position = Vector2.Lerp(firstPos, targetPos, elapsed/duration);
+                
+                yield return null;
+            }
+        }
+    }
 }
